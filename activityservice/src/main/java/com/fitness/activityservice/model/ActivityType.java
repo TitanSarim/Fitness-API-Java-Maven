b04e13626207c79e9d82bc9649be448d0f891f31
TitanSarim <muhammadsarim@gmail.com>
2025-11-05 11:51:04 +0300

    activity service

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,12 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNIGN,
+    WALKING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING
+}
